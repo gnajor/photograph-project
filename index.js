99f8a197714d656c60f40b04a4a2a4bd5ca9469f
gnajor <lejonet04@gmail.com>
2023-12-03 13:30:18 +0100

    cleaned up

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function select_nav_item(){
 
+    let div_dom = document.querySelector(".nav_item:nth-of-type(3)");
+    div_dom.addEventListener("click", on_click_take_picture);
+
+    function on_click_take_picture(){
+        document.body.style.backgroundImage = "url(media/background2.jpg)";
+    }
+}
+
+select_nav_item();
