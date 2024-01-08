c70773022a8ed955e77048ebbc751544b29c9244
Sabka13 <143510385+Sabka13@users.noreply.github.com>
2024-01-08 14:16:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,20 +61,21 @@ function on_click_shutter() {
 
     let popup_container = document.querySelector("#popup_container");
 
-    shutter_effect.classList.add("shutter");}
+    shutter_effect.classList.add("shutter");
+}
 
-    if (container.style.right === "100%") { 
-        // Här ska history popupen vara
-        let historia_popup = document.querySelector("#historia_wrapper");
-        shutter_effect.addEventListener("transitionend", function () {
-            historia_popup.style.display = "grid";
-        });
+if (container.style.right === "100%") {
+    // Här ska history popupen vara
+    let historia_popup = document.querySelector("#historia_wrapper");
+    shutter_effect.addEventListener("transitionend", function () {
+        historia_popup.style.display = "grid";
+    });
     if (container.style.right === "100%") {
         let historia_popup = document.querySelector("#historia_popup");
         historia_popup.style.display = "flex";
-    
+
         let remove = historia_popup.querySelector(".remove_button");
-        remove.addEventListener("click", function(){
+        remove.addEventListener("click", function () {
             historia_popup.style.display = "none";
             shutter_effect.classList.remove("shutter");
         });
@@ -82,7 +83,7 @@ function on_click_shutter() {
 
     if (container.style.right === "200%") {
         // Här ska nutid popupen vara
-        let nutid_popup = document.querySelector("#nutid_wrapper");
+        let nutid_wrapper = document.querySelector("#nutid_wrapper");
         nutid_wrapper.style.display = "flex";
 
 
